275ae80c0999f2ac07a17713dd2c33f8f0c65a82
davidwilkins <davidwilkins@davids-mbp-2>
2017-10-09 22:04:06 -0400

    added text file reader;

--- src/utilities/IO.java
+++ src/utilities/IO.java
@@ -1,11 +1,41 @@
 package utilities;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class IO {
     
-    public static void readFromFile(String fileName) {
-        System.out.println("utilities.IO.readFromFile()::start::");
+    
+     
+    public static List<String> readFromTextFile(File f) throws Exception {
+        List<String> fileContent = new ArrayList();
+
+        InputStream inStrm = new FileInputStream(f);
+        Reader rdr = new InputStreamReader(inStrm);
+        BufferedReader bufRdr = new BufferedReader(rdr);
+
+        while (true) {
+            String line = bufRdr.readLine();
+            if (line == null) break;
+            line = line.replace(String.valueOf((char) 0xFEFF), "");
+            fileContent.add(line);
+        }
+
+        return fileContent;
     }
+    
+     public static List<String> readTextFile(String fileName) throws Exception {
+         File f = new File(fileName);
+         List<String> fileContent = readFromTextFile(f);
+         return fileContent;
+     }
+    
     
 }
