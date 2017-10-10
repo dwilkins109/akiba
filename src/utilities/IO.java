d1415dcc8cb8a9aa62599a80e19f267a78ef2b19
davidwilkins <davidwilkins@davids-mbp-2>
2017-10-09 20:13:25 -0400

    IO branch commit 002;

---
+++ src/utilities/IO.java
@@ -0,0 +1,11 @@
+package utilities;
+
+
+
+public class IO {
+    
+    public static void readFromFile(String fileName) {
+        System.out.println("utilities.IO.readFromFile()::start::");
+    }
+    
+}
